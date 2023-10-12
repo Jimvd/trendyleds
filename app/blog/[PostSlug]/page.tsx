892689df07719645a629ced5691fd2d8a2ddf622
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2023-10-12 22:13:35 +0200

    added singlepost page but not working

--- app/blog/[PostSlug]/page.tsx
+++ app/blog/[PostSlug]/page.tsx
@@ -1,3 +1,29 @@
-export default function Post() {
-   return <h1>This is a post</h1>;
+import SiteHeader from "@/components/navigation/MainMenu/navigation";
+import { query } from "@/lib/graphql/lib/query";
+import { GET_CONTENT } from "@/lib/graphql/singlepost";
+
+const getSinglePost = async (slug: string) => {
+   const { data } = await query({
+      query: GET_CONTENT,
+      variables: {
+         slug: slug,
+      },
+      revalidate: true,
+   });
+
+   if (!data.post) throw new Error("Kon berichten niet ophalen");
+
+   return data.post;
+};
+
+export default async function singlePost({ params }: { params: { PostSlug: string } }) {
+   const post = await getSinglePost(params.PostSlug);
+
+   return (
+      <>
+         <SiteHeader />
+
+         {post.title}
+      </>
+   );
 }
