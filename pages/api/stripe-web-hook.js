5ca3a6b4ec8bc28c41f1379755bf308e3d16cf04
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 21:02:11 +0200

    try in json

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -11,7 +11,6 @@ export const config = {
       bodyParser: false,
    },
 };
-
 export default async function handler(req, res) {
    const buf = await buffer(req);
    const sig = req.headers["stripe-signature"];
@@ -21,7 +20,8 @@ export default async function handler(req, res) {
       console.log("Received Stripe event: " + JSON.stringify(stripeEvent));
 
       if ("checkout.session.completed" === stripeEvent.type) {
-         return res.status(200).json({ received: true });
+         // Return the exact received payload as JSON response
+         return res.status(200).send(payload);
       } else {
          return res.status(405).json({ error: "Method Not Allowed" });
       }
