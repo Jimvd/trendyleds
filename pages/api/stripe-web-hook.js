8c20a29b18e38e8a339c051c8d95a269edf31242
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 20:45:40 +0200

    added default function

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
    apiVersion: "2023-10-16",
 });
 
-export async function POST(req) {
+export default async function POST(req) {
    const buf = await req.text();
    const sig = req.headers.get("stripe-signature");
    let event;
