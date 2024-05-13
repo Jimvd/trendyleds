88babc23f0318074a532a0d632ab3773653da3b1
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 20:40:04 +0200

    test

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -1,71 +1,16 @@
-import { Stripe } from "stripe";
-import axios from "axios";
-
-import { buffer } from "micro";
-
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
    apiVersion: "2023-10-16",
 });
-const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
-export const config = {
-   api: {
-      bodyParser: false,
-   },
-};
-const createOrder = async (orderData) => {
-   try {
-      const response = await axios.post("/createOrder", orderData);
-      console.log("Order created: " + JSON.stringify(response.data));
-   } catch (error) {
-      console.log("Error creating order: " + error.message);
-   }
-};
 
-export default async function handler(req, res) {
-   const buf = await buffer(req);
-   const sig = req.headers["stripe-signature"];
+export async function POST(req) {
+   const buf = await req.text();
+   const sig = req.headers.get("stripe-signature");
+   let event;
    try {
-      const payload = buf.toString();
-      const stripeEvent = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
-      console.log("Received Stripe event: " + JSON.stringify(stripeEvent));
-
-      if ("checkout.session.completed" === stripeEvent.type) {
-         const testData = {
-            payment_method: "bacs",
-            payment_method_title: "Direct Bank Transfer",
-            set_paid: true,
-            billing: {
-               first_name: "Jim",
-               last_name: "Doe",
-               address_1: "123 Main St",
-               city: "Anytown",
-               postcode: "12345",
-               country: "US",
-               email: "john.doe@example.com",
-               phone: "123456789",
-            },
-            shipping: {
-               first_name: "John",
-               last_name: "Doe",
-               address_1: "123 Main St",
-               city: "Anytown",
-               postcode: "12345",
-               country: "US",
-            },
-            line_items: [
-               {
-                  product_id: 1,
-                  quantity: 2,
-               },
-            ],
-         };
-         await createOrder(testData);
-         return res.status(200).json({ received: true });
-      } else {
-         return res.status(405).json({ error: "Method Not Allowed" });
-      }
+      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
    } catch (err) {
-      console.log("Webhook Error: " + err.message);
-      return res.status(400).json({ error: `Webhook Error: ${err.message}` });
+      console.log(`Error: Stripe webhook failed: ${err.message}`);
+      return NextResponse.json({ received: false, error: err.message }, { status: 400 });
    }
 }
