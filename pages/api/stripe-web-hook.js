522fc1502a55f14bb0b29a95a7114f9e382442d1
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 21:27:13 +0200

    back to basic

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -1,26 +1,27 @@
 import { Stripe } from "stripe";
+import axios from "axios";
+import { buffer } from "micro";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
    apiVersion: "2023-10-16",
 });
 const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
+
 export const config = {
    api: {
       bodyParser: false,
    },
 };
+
 export default async function handler(req, res) {
+   const buf = await buffer(req);
    const sig = req.headers["stripe-signature"];
-
-   let stripeEvent;
-
    try {
-      stripeEvent = stripe.webhooks.constructEvent(sig, webhookSecret);
+      const payload = buf.toString();
+      const stripeEvent = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
       console.log("Received Stripe event: " + JSON.stringify(stripeEvent));
    } catch (err) {
       console.log("Webhook Error: " + err.message);
       return res.status(400).json({ error: `Webhook Error: ${err.message}` });
    }
-
-   res.send();
 }
