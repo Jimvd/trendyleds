4060ab975e4e6c6449700c66ddbb01963473ed5b
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 20:53:48 +0200

    added buffer

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -1,18 +1,21 @@
 import { Stripe } from "stripe";
-
+import { buffer } from "micro";
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
    apiVersion: "2023-10-16",
 });
 
 export default async function POST(req) {
-   const buf = req.body;
+   const buf = await buffer(req);
+
    const sig = req.headers["stripe-signature"];
    let event;
    try {
       event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
    } catch (err) {
       console.log(`Error: Stripe webhook failed: ${err.message}`);
-      return NextResponse.json({ received: false, error: err.message }, { status: 400 });
+      return res.status(400).json({ received: false, error: err.message });
    }
+   // Handle the event
+   res.json({ received: true });
 }
