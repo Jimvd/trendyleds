111c757de6c66dd4f7eb34b377f239b3e216cc01
jimvd <jim.van.duijkeren@schaapcitroen.nl>
2024-05-13 21:57:50 +0200

    try webhook stripe

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -1,27 +1,44 @@
 import { Stripe } from "stripe";
-import { buffer } from "micro";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
    apiVersion: "2023-10-16",
 });
-const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
 
-export const config = {
-   api: {
-      bodyParser: false,
-   },
-};
+export default async function POST(req) {
+   const body = await req.text();
+   const sig = req.headers.get("stripe-signature");
+   const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
+   let event;
 
-export default async function handler(req, res) {
-   const buf = await buffer(req);
-   const sig = req.headers["stripe-signature"];
    try {
-      const stripeEvent = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-      console.log("Received Stripe event: " + JSON.stringify(stripeEvent));
-
-      res.status(200).json({ received: true });
+      if (!sig || !webhookSecret) return new Response("Webhook secret not found.", { status: 400 });
+      event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+      console.log(`🔔  Webhook received: ${event.type}`);
    } catch (err) {
-      console.log("Webhook Error: " + err.message);
-      return res.status(400).json({ error: `Webhook Error: ${err.message}` });
+      console.log(`❌ Error message: ${err.message}`);
+      return new Response(`Webhook Error: ${err.message}`, { status: 400 });
    }
+
+   if (relevantEvents.has(event.type)) {
+      try {
+         switch (event.type) {
+            case "checkout.session.completed":
+               const checkoutSession = event.data.object;
+               console.log(checkoutSession);
+               break;
+            default:
+               throw new Error("Unhandled relevant event!");
+         }
+      } catch (error) {
+         console.log(error);
+         return new Response("Webhook handler failed. View your Next.js function logs.", {
+            status: 400,
+         });
+      }
+   } else {
+      return new Response(`Unsupported event type: ${event.type}`, {
+         status: 400,
+      });
+   }
+   return new Response(JSON.stringify({ received: true }));
 }
